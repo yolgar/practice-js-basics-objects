68bcfcba2f500ff0023a930163bd9fea9567ba58
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-07-12 17:28:47 +0200

    finish 04

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+const user = {
+    firstName: 'Anna',
+    lastName:'Anowicz',
+    sex:'Female',
+    age: 43,
+};
+
+console.log(user['firstName'], user.lastName, user.sex, user.age);
--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,21 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '12',
+        month: '07',
         year: '1985'
     }
 }
+
+console.log(parseInt(user.born.day), parseInt(user.born.month));
+
+const currentDate = new Date();
+const currentDay = currentDate.getDate();
+const currentMonth = currentDate.getMonth() + 1;
+console.log(currentDate, currentDay, currentMonth);
+
+if(parseInt(user.born.day) === currentDay && parseInt(user.born.month) === currentMonth){
+    console.log('HAPPY BIRTHDAY, ' + user.firstName + ' ' + user.lastName);
+} 
+
+
